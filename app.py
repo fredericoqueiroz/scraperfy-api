0f6af9137dcbb929f40477d82ec02acdca15035a
fredericoqueiroz <fredqueirozbh@gmail.com>
2021-02-17 18:12:36 -0300

    Refactored Flask Config

---
+++ app.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+from flask import Flask, jsonify
+from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s]: {} %(levelname)s %(message)s'.format(os.getpid()),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.StreamHandler()])
+
+logger = logging.getLogger()
+
+
+db = SQLAlchemy()
+
+
+def create_app():
+    logger.info(f'Starting app in {config.APP_ENV} environment')
+
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    api = Api(app, version='1.0', title='Scraperfy API',
+        description='A RESTful API that serves data from the Scraperfy Package'
+    )
+
+    # initialize SQLAlchemy
+    db.init_app(app)
+
+    @app.route('/health')
+    def health():
+        return jsonify('healthy')
+    
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
---
+++ config/settings.py
@@ -0,0 +1,24 @@
+class BaseConfig():
+    API_PREFIX = '/scraperfy'
+    TESTING = False
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevConfig(BaseConfig):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testeErro:fred@localhost/scraperfy_dev?charset=utf8mb4'
+    SQLALCHEMY_ECHO = True
+
+class TestConfig(BaseConfig):
+    CONFIG_NAME = 'test'
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fred:fred@localhost/scraperfy_test?charset=utf8mb4'
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(BaseConfig):
+    FLASK_ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fred:fred@localhost/scraperfy_prod?charset=utf8mb4'
+
