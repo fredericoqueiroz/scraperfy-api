ebd641b4ed75eb6e5ffa896ec5c7b1851fff0271
fredericoqueiroz <fredqueirozbh@gmail.com>
2021-03-10 17:23:00 -0300

    Created requirements. Migrating to Linux base

--- financial_data/extensions/tasks/tasks.py
+++ financial_data/extensions/tasks/tasks.py
@@ -5,5 +5,5 @@ from.current_price import smoke_task, get_current_price_task
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(60.0, get_current_price_task, name='current price')
-    sender.add_periodic_task(60.0, smoke_task, name='smoke task')
+    sender.add_periodic_task(10.0, smoke_task, name='smoke task')
 
