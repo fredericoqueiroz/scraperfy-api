b0ad1242156e75613681a63cd5573b8a045b033c
fredericoqueiroz <fredqueirozbh@gmail.com>
2021-07-22 15:54:42 -0300

    Change webdriver to geckodriver

--- financial_data/tasks/current_price.py
+++ financial_data/tasks/current_price.py
@@ -1,7 +1,7 @@
 from financial_data.blueprint.scraperfy_api.asset_scraper.current_price.model import \
     CurrentPrice
 from financial_data.extensions.database import db
-from financial_data.extensions.webdriver import init_chrome_driver
+from financial_data.extensions.webdriver import init_firefox_driver
 from scraperfy.intraday_scraper import current_price as cp
 
 from . import celery
@@ -11,7 +11,7 @@ from .celery_worker import celery, flask_app
 @celery.task(name='current_price_task')
 def get_current_price_task(asset):
 
-    driver = init_chrome_driver()
+    driver = init_firefox_driver()
 
     price = cp.CurrentPrice(driver, asset)
 
@@ -25,5 +25,3 @@ def get_current_price_task(asset):
     with flask_app.app_context():
         db.session.add(new_cp)
         db.session.commit()
-
-    #return new_cp
