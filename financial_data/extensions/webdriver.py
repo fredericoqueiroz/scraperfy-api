6cb17656a556bb28a6b6d6ccfa3162b5296d1abc
fredericoqueiroz <fredqueirozbh@gmail.com>
2021-07-22 15:42:40 -0300

    Add Firefox Webdriver (geckodriver)

--- financial_data/extensions/webdriver.py
+++ financial_data/extensions/webdriver.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-PATH = "C:/Program Files (x86)/chromedriver.exe"
+CHROME_PATH = "C:/Program Files (x86)/chromedriver.exe"
+FIREFOX_PATH = "C:/Program Files (x86)/geckodriver.exe"
 
-def init_chrome_driver(path=PATH):
+def init_chrome_driver(path=CHROME_PATH):
 
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
 
-    driver = webdriver.Chrome(PATH, options=op)
+    driver = webdriver.Chrome(path, options=op)
+
+    return driver
+
+def init_firefox_driver(path=FIREFOX_PATH):
+
+    op = Options()
+    op.add_argument('--headless')
+
+    driver = webdriver.Firefox(executable_path=path, options=op)
 
     return driver
